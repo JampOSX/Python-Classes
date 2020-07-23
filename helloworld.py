ccb10414f14b17f472391414d6f5c33395329835
JampOSX <jmejiap@gmail.com>
2020-07-22 23:10:09 -0400

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+import os
+os.system('cls')
+
+# Print Hello World to the Screen
+'''print ("Hello world!")
+'''
+
+#First code with variable
+
+full_name = "JampOSX"
+
+print (full_name)
+
+full_name = "JlMP"
+
+print (full_name)
+
+last_name = "Mejia"
+
+full_name = last_name
+
+print (full_name)
